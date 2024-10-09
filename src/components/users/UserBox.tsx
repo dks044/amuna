d685c4d2d39993f94a13699b2682bc5c33608e27
AJN <buzz781@gmail.com>
2024-10-10 04:10:16 +0900

    feat: conversation API 구현

---
+++ src/components/users/UserBox.tsx
@@ -0,0 +1,67 @@
+'use client';
+import { User } from '@prisma/client';
+import axios from 'axios';
+import { useRouter } from 'next/router';
+import React, { useState } from 'react';
+import toast from 'react-hot-toast';
+import LoadingModal from '../modals/LoadingModal';
+import clsx from 'clsx';
+import Avata from '../Avata';
+
+interface UserBoxProps {
+  data: User;
+}
+
+const UserBox = ({ data }: UserBoxProps) => {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+  const handleClick = () => {
+    setIsLoading(true);
+
+    axios
+      .post('api/conversations', { userId: data.id })
+      .then(data => {
+        router.push(`/conversations/${data.data.id}`); //conversationId
+      })
+      .catch(error => {
+        toast.error('에러가 발생했습니다!');
+        console.log(error);
+      })
+      .finally(() => setIsLoading(false));
+  };
+
+  return (
+    <>
+      {isLoading && <LoadingModal show={isLoading} />}
+      <div
+        onClick={handleClick}
+        className={clsx(
+          `
+          w-full
+          relative
+          flex
+          items-center
+          space-x-3
+          p-3
+          hover:bg-neutral-100
+          rounded-lg
+          transition
+          cursor-pointer
+        `,
+        )}
+      >
+        <Avata />
+        <div className='min-w-0 flex-1'>
+          <div className='focus:outline-none'>
+            <div className='flex justify-between items-center mb-1'>
+              {/* userName */}
+              <p className='text-sm font-medium text-gray-900'>{data.name}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default UserBox;
